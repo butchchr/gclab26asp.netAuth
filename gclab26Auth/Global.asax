cec4f3d633e8a9e12801bf39cce7c413710686f8
chris <butchchr@gmail.com>
2018-08-28 21:59:37 -0400

    Add project files.

---
+++ gclab26Auth/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="gclab26Auth.MvcApplication" Language="C#" %>
---
+++ gclab26Auth/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(gclab26Auth.Startup))]
+namespace gclab26Auth
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
